55467f83f02791b7d2310381f8f69097fe7d4bcd
Mubashir Ahmad <ahmad_14p@hotmail.com>
2019-10-20 20:55:13 +0500

    Add project files.

---
+++ CVeator.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CVeator.Web.MvcApplication" Language="C#" %>
---
+++ CVeator.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CVeator.Web.Startup))]
+namespace CVeator.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
